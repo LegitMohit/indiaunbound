ea9f2b22b5b299137316dfb36b7ccf77ed793cf1
LegitMohit <msharma7744@gmail.com>
2024-10-15 12:25:03 +0530

    Add files via upload

---
+++ exploringnorth/index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/x-icon" href="../images/India-Unbound-logo.png">
+    <link href="https://fonts.cdnfonts.com/css/samsung-sans-4" rel="stylesheet" />
+    <link rel="stylesheet" href="../css/style_collection.css" type="text/css">
+    <link rel="stylesheet" href="../css/style_collection_images.css">
+
+    <title>Exploring North</title>
+</head>
+<body>
+    <header>
+    <?php
+        require('../index header.php');
+    ?>
+    </header>
+    <main>
+    <P class="collection-heading">Exploring NORTH For You</P> <br>
+        <div class="info_div">
+            <div class="information_div">
+                <div class="image_container image_container_left image_kashmirnorth">
+                </div>
+                <ol class="details_ol image_container_right">
+                    <li>
+                        <h3>
+                            Kashmir
+                        </h3>
+                        <ul>
+                            <li>Famous for: breathtaking landscapes, including Dal Lake, Mughal gardens, and snow-capped mountains. Offers adventure activities like skiing, trekking, and houseboat stays.</li>
+                            <li>Best time to visit: April to October</li>
+                            <li>Kashmir, often called Paradise on Earth, is a breathtaking region in India. Known for its stunning natural beauty, including snow-capped mountains, crystal-clear lakes, and lush green valleys, it's a dream destination for nature lovers. Srinagar, the capital, offers houseboat stays on Dal Lake, while Gulmarg is a winter wonderland for skiing. However, due to geopolitical tensions, it's essential to check travel advisories before planning a trip.</li>
+                        </ul>
+                    </li>
+                </ol>
+            </div>
+            <div class="flip_div">
+                <div class="image_container image_container_right image_rajasthannorth">
+                </div>
+                <ol class="details_ol image_container_left" start="2">
+                    <li>
+                        <h3>
+                            Rajasthan
+                        </h3>
+                    <ul>
+                        <li>Famous for: Royal forts, palaces, vibrant culture, and deserts. Experience the charm of cities like Jaipur, Udaipur, Jodhpur, and Jaisalmer.</li>
+                        <li>Best time to visit: October to March</li>
+                        <li>Rajasthan, the "Land of Kings," is a treasure trove of Indian heritage. Known for its majestic forts and palaces, the state boasts cities like Jaipur, Udaipur, and Jodhpur, each with its unique charm. Experience the vibrant culture with folk dances, music, and handicrafts. Explore the Thar Desert, ride camels, and stay in luxurious heritage hotels. Rajasthan offers a perfect blend of history, culture, and adventure.</li>
+                    </ul>
+                </li>
+                </ol>
+            </div>
+            <div class="information_div">
+                <div class="image_container image_container_left image_Himachal">
+                </div>
+                <ol class="details_ol image_container_right" start="3">
+                    <li>
+                        <h3>
+                            Himachal Pradesh
+                        </h3>
+                    
+                    <ul>
+                        <li>Famous for: Hill stations like Shimla, Manali, and Kullu, offering stunning natural beauty, adventure sports, and a peaceful atmosphere.   </li>
+                        <li>Best time to visit: March to June and September to November</li>
+                        <li>Himachal Pradesh, often called "Devbhoomi" or "Land of Gods," is a haven for nature lovers. This northern Indian state boasts snow-capped mountains, lush valleys, and pristine rivers. Popular hill stations like Shimla, Manali, and Kullu offer breathtaking views and adventure sports. Trek through dense forests, relax amidst serene lakes, or experience the thrill of skiing. Himachal Pradesh is a perfect escape for those seeking tranquility and natural beauty.</li>
+                    </ul>
+                </li>
+                </ol>                
+            </div>
+        </div>
+    </div>
+    </main>
+    <footer>
+        <?php
+            require_once('../index footer.php');
+        ?>
+    </footer>
+</body>
+</html>
